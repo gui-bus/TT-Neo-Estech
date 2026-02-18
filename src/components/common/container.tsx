bf33e140ac80f4cdb1e5ff2bdbcf81bed1d9a5b8
Guilherme Bustamante <guilherme_bustamante@live.com>
2026-02-18 19:10:51 -0300

    feat: add container component

---
+++ src/components/common/container.tsx
@@ -0,0 +1,22 @@
+//#region Imports
+import { cn } from "@/src/lib/utils/utils";
+import type { ReactNode } from "react";
+//#endregion
+
+//#region Interfaces
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+  id?: string;
+}
+//#endregion
+
+const Container = ({ children, className, id }: ContainerProps) => {
+  return (
+    <main className={cn("p-5 lg:p-10", className)} id={id}>
+      {children}
+    </main>
+  );
+};
+
+export default Container;
