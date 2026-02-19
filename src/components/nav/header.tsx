765ee8a57d47221552e33ec4210b3ce1bfee9dd0
Guilherme Bustamante <guilherme_bustamante@live.com>
2026-02-19 17:52:21 -0300

    style: change header design

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitcher from "@/src/components/theme/themeSwitcher";
+import { Avatar } from "antd";
 //#endregion
 
 export const Header = () => {
@@ -13,18 +14,20 @@ export const Header = () => {
         <Image src="/neoestech.png" alt="Logo" width={50} height={50} />
       </Link>
 
-      {/* LINKS - THEME SWITCHER */}
-      <div className="flex items-center gap-5 divide-x">
-        {/* LINKS */}
-        <Link
-          href="/chamados"
-          className="hover:-translate-y-1 transition-all duration-300 hover:text-brand! text-black! dark:text-white! pr-8"
-        >
-          Chamados
-        </Link>
-
+      {/* THEME SWITCHER - AVATAR */}
+      <div className="flex items-center gap-5">
         {/* THEME SWITCHER */}
         <ThemeSwitcher />
+
+        {/* AVATAR */}
+        <div className="flex items-center gap-2">
+          <Avatar size="large" src="https://i.pravatar.cc/150?img=68" />
+
+          <div className="flex flex-col cursor-default">
+            <span className="text-xs">John Doe</span>
+            <span className="text-xs opacity-50">johndoe@email.com</span>
+          </div>
+        </div>
       </div>
     </header>
   );
